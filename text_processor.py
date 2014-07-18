4c89d760e26e017813196747fa91eb50b3cbf610
Aleks Navratil <anavratil@collective.com>
2014-07-18 16:43:17 -0400

    Just ran it with threshold = 90

--- text_processor.py
+++ text_processor.py
@@ -11,7 +11,7 @@ master_list = read_csv("all SF accounts_caro.csv")
 #slave_list = slave_list[0:100]
 #master_list = master_list[0:500] ## Use these two lines to reduce the size of your data. Useful for fast testing and protoyping new features
 
-threshold_value = 90 ## Tune this by hand to get good agreement. Probably you want a value between 70 and 90. It works like this:
+threshold_value = 75  ## Tune this by hand to get good agreement. Probably you want a value between 70 and 90. It works like this:
 #>>> fuzz.partial_ratio("this is a test", "this is a test!")
 #    100
 
